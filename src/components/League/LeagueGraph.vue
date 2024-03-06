5c4fe1c01bd04c1acffe8cc64ba332db8343cadc
Philipp Buchholz <pbuchholz87@gmail.com>
2024-03-07 00:28:38 +0100

    Update: Frontend LeagueGraph

--- src/components/League/LeagueGraph.vue
+++ src/components/League/LeagueGraph.vue
@@ -14,11 +14,14 @@ const props = defineProps<{
 
 const colors = [
     "FireBrick",
-    "Green", "Blue", "DarkOrchid", "Brown",
+    // "Green",
+    "Blue",
+    "DarkOrchid",
+    "HotPink",
+    "Brown",
     "DarkMagenta",
     "Crimson",
     "DarkGreen",
-    "HotPink",
     "Grey",
     "DarkBlue",
     "Gold",
@@ -31,6 +34,7 @@ export interface DataSet {
     borderColor?: string,
     data: { x: number, y: number, tooltip: string }[]
     hidden: boolean,
+    pointRadius: number
 }
 
 const graphData = () => {
@@ -66,20 +70,24 @@ const graphData = () => {
 };
 
 const yAxis = [
-    { url: `image/ranked/diamond.webp`, color: "#4665B5" },
-    { url: `image/ranked/emerald.webp`, color: "#067066" },
-    { url: `image/ranked/platinum.webp`, color: "#3F857E" },
-    { url: `image/ranked/gold.webp`, color: "#BD811A" },
-    { url: `image/ranked/silver.webp`, color: "#999999" },
-    { url: `image/ranked/bronze.webp`, color: "#9C5E38" },
-].map((img) => {
+    { url: `image/ranked/diamond.webp`, color: "#4665B5", start: 2400 },
+    { url: `image/ranked/emerald.webp`, color: "#067066", start: 2000 },
+    { url: `image/ranked/platinum.webp`, color: "#3F857E", start: 1600 },
+    { url: `image/ranked/gold.webp`, color: "#BD811A", start: 1200 },
+    { url: `image/ranked/silver.webp`, color: "#999999", start: 800 },
+    // { url: `image/ranked/bronze.webp`, color: "#9C5E38", start: 400 },
+].map((yAxis) => {
     const image = new Image();
-    image.src = img.url;
-    return { image, color: img.color, url: img.url };
+    image.src = yAxis.url;
+    return { image, ...yAxis };
 });
 
 onMounted(() => {
     const chartContainer: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("myChart");
+    const firstGamesThisSeason = props.summoners
+        .map((sum) => sum?.ranking?.soloQueueRanks?.pop()?.timestamp || 0).filter((num) => num !== 0);
+    const firstTimestamp = Math.min(...firstGamesThisSeason) * 1000;
+
     const plugin = {
         id: "custom_canvas_background_image",
         legend: { display: false },
@@ -90,49 +98,58 @@ onMounted(() => {
 
                     const { width, height } = chart.chartArea;
                     const barHeight = height / arr.length;
-                    const x1 = 0;
                     const y1 = 5 + (index * barHeight);
-                    const widthOffset = 70;
+                    const widthOffset = 80;
+                    const fontSize = 12;
 
                     // draw backgrounds
                     ctx.globalAlpha = 0.5;
                     ctx.fillStyle = yAxis.color;
-                    ctx.fillRect(x1, y1, width + widthOffset + 10, barHeight);
+                    ctx.fillRect(0, y1, width + widthOffset, barHeight);
                     ctx.globalAlpha = 1;
 
                     // draw devision lines
-                    ctx.font = "14px Roboto";
+                    ctx.font = `${fontSize}px Roboto`;
                     ctx.strokeStyle = "#999";
 
-                    ctx.fillText("I", x1 + widthOffset - 10, y1 + 0.25 * barHeight);
-                    ctx.fillText("II", x1 + widthOffset - 10, y1 + 0.5 * barHeight);
-                    ctx.fillText("III", x1 + widthOffset - 10, y1 + 0.75 * barHeight);
-                    if (index !== 3) {
-                        ctx.fillText("IV", x1 + widthOffset - 10, y1 + barHeight);
-                    }
+                    ctx.fillText("I", widthOffset, y1 + (0.25 * barHeight) - fontSize / 2);
+                    ctx.fillText("II", widthOffset, y1 + (0.5 * barHeight) - fontSize / 2);
+                    ctx.fillText("III", widthOffset, y1 + (0.75 * barHeight) - fontSize / 2);
+                    ctx.fillText("IV", widthOffset, y1 + barHeight - fontSize / 2);
+
                     ctx.beginPath();
-                    ctx.moveTo(x1 + widthOffset + 10, y1 + 0.25 * barHeight);
-                    ctx.lineTo(width + widthOffset + 10, y1 + 0.25 * barHeight);
+                    ctx.moveTo(widthOffset, y1 + 0.25 * barHeight);
+                    ctx.lineTo(width + widthOffset, y1 + 0.25 * barHeight);
 
-                    ctx.moveTo(x1 + widthOffset + 10, y1 + 0.5 * barHeight);
-                    ctx.lineTo(width + widthOffset + 10, y1 + 0.5 * barHeight);
+                    ctx.moveTo(widthOffset, y1 + 0.5 * barHeight);
+                    ctx.lineTo(width + widthOffset, y1 + 0.5 * barHeight);
 
-                    ctx.moveTo(x1 + widthOffset + 10, y1 + 0.75 * barHeight);
-                    ctx.lineTo(width + widthOffset + 10, y1 + 0.75 * barHeight);
+                    ctx.moveTo(widthOffset, y1 + 0.75 * barHeight);
+                    ctx.lineTo(width + widthOffset, y1 + 0.75 * barHeight);
 
-                    ctx.moveTo(x1 + widthOffset + 10, y1 + barHeight);
-                    ctx.lineTo(width + widthOffset + 10, y1 + barHeight);
+                    ctx.moveTo(widthOffset, y1 + barHeight);
+                    if (index !== arr.length - 1) ctx.lineTo(width + widthOffset, y1 + barHeight);
                     ctx.stroke();
 
                     // draw emblems
-                    ctx.drawImage(yAxis.image, x1 + 5, y1, height / arr.length, height / arr.length);
+                    const imageScale = (100 - index * 3) / 100;
+                    const imageSize = widthOffset * imageScale;
+
+                    ctx.drawImage(
+                        yAxis.image,
+                        index,
+                        y1 + (barHeight / 2) - (widthOffset) / 2,
+                        imageSize,
+                        imageSize,
+                    );
                     ctx.restore();
                 });
             }
         },
     };
     if (chartContainer) {
-        const myChart = new Chart(chartContainer as HTMLCanvasElement, {
+        const paddingLeft = 60 * 60 * 24 * 7 * 800;
+        new Chart(chartContainer as HTMLCanvasElement, {
             type: "line",
             data: graphData(),
             options: {
@@ -156,8 +173,8 @@ onMounted(() => {
                             display: true,
                         },
                         beginAtZero: false,
-                        min: 400,
-                        max: 2800,
+                        min: Math.min(...yAxis.map((y) => y.start)),
+                        max: Math.max(...yAxis.map((y) => y.start)) + 400,
                         ticks: {
                             stepSize: 400,
                         },
@@ -172,7 +189,7 @@ onMounted(() => {
                         grid: {
                             display: true,
                         },
-                        min: 1688169600000, // Monday, 10. January 2022 00:00:00
+                        min: (firstTimestamp) - paddingLeft,
                     },
                 },
                 layout: {
